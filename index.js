7dd1be5eb15171a13ea90da9bbe2fa1d79ba4456
SergDv1962 <dsv19621227@gmail.com>
2024-08-29 17:26:09 +0300

    commit after test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import userRouter from './routes/user.js';
-
+import 'dotenv/config'
 
 const app = express();
 
@@ -14,11 +14,8 @@ app.use(cors());
 
 app.use('/users', userRouter);
 
-
-const port = 5000;
-
-mongoose.connect(MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => {
-    app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
   })
   .catch((error) => console.log(`${error} did not connect`));
